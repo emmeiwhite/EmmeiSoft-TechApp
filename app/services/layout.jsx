8189b9ab1e6fe5fd2b36783fd3d959a4bedf0321
emmeiwhite <emmeiwhite@gmail.com>
2025-06-24 15:09:28 +0530

    Nested Routes for Services - hands-on

---
+++ app/services/layout.jsx
@@ -0,0 +1,8 @@
+function layout({ children }) {
+  return (
+    <div className="py-5 max-w-5xl mx-auto px-6">
+      <div>{children}</div>
+    </div>
+  )
+}
+export default layout
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,28 @@
+import Link from 'next/link'
+
 function ServicesPage() {
   return (
-    <div className="max-w-5xl mx-auto px-6 min-h-screen">
+    <div className="min-h-screen">
       <div className="py-8">
         <h1 className="text-2xl font-semibold"> EmmeiSoft Tech | All Services</h1>
+
+        <div className="mt-8 flex flex-col gap-3">
+          <Link
+            className="text-orange-500 hover:text-orange-700 transition-all"
+            href="/services/web-dev">
+            Web Dev
+          </Link>
+          <Link
+            className="text-orange-500 hover:text-orange-700 transition-all"
+            href="/services/app-dev">
+            App Dev
+          </Link>
+          <Link
+            className="text-orange-500 hover:text-orange-700 transition-all"
+            href="/services/ui-ux">
+            UI/UX
+          </Link>
+        </div>
       </div>
     </div>
   )
