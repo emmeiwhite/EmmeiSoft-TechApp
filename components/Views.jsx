79b20e48fae37e983d530355da7fb02b6945a790
emmeiwhite <emmeiwhite@gmail.com>
2025-06-25 06:47:25 +0530

    <Views> is an async Client Component. Only Server Components can be async at the moment.

--- components/Views.jsx
+++ components/Views.jsx
@@ -1,6 +1,8 @@
+'use client'
+
 export default async function Views() {
   // Mock API call so that the components renders with a delay
-  await new Promise(resolve => setTimeout(resolve, 3000))
+  //   await new Promise(resolve => setTimeout(resolve, 3000))
 
   return <div>10K Views</div>
 }
