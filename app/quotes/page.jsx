ed082f2299b9318ef6f723ea8f042c3fa6c0a6bf
emmeiwhite <emmeiwhite@gmail.com>
2025-06-25 08:55:54 +0530

    Error: Event handlers cannot be passed to Client Component props.

--- app/quotes/page.jsx
+++ app/quotes/page.jsx
@@ -6,6 +6,14 @@ function QuotesPage() {
     <div className="max-w-5xl mx-auto px-6 min-h-screen">
       <div className="py-8">
         <h1 className="text-2xl font-semibold"> Quotes Page </h1>
+
+        <button
+          className="bg-teal-300 px-3 py-2 rounded"
+          onClick={() => {
+            console.log('I was clicked')
+          }}>
+          Click Me
+        </button>
         <p className="mt-5">
           Quotes from the people of wisdom are full of wisdom, let's get ourselves acquianted with
           few quotes of wise people
